03e9d8ca8ea4ade6a2267744eb04e655baa2acf3
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2025-04-12 21:17:43 -0300

    cantreva

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const contador = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <div className='container'>
+        <img
+          src='/images/cantreva.jpg'
+          alt='Cantreva'
+          className='cantreva-img'
+        />
+        <div className='content'>
+          <h1>Quantas vezes cantreva já mamou hoje?</h1>
+          <div className='horizontal-group'>
+            <div className='counter-text'>Quantidade: {count}</div>
+            <button className='button' onClick={contador}>
+              Mamadas
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
